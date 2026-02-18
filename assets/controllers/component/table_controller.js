2be07061f270c124e351f38ae91098ae574032cf
Florent HAZARD <f.hazard@sowapps.com>
2026-02-18 15:59:09 +0100

    ✨ Empty SoCore table component in replacement of DataTable

---
+++ assets/controllers/component/table_controller.js
@@ -0,0 +1,9 @@
+import {AbstractController} from "../../core/controller/abstract.controller.js";
+
+export default class extends AbstractController {
+	
+	initialize() {
+		// TODO implement or find a library
+	}
+	
+}
